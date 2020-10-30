edf288d0216d58572ecf8c5852113a196622e8da
Khai Do <zaro0508@gmail.com>
2020-10-30 13:54:24 -0700

    add more tests

--- tests/unit/utils/test_get_marketplace_tags.py
+++ tests/unit/utils/test_get_marketplace_tags.py
@@ -7,7 +7,7 @@ from botocore.stub import Stubber
 
 class TestGetMarketplaceTags(unittest.TestCase):
 
-  def test_happy_path(self):
+  def test_has_tags(self):
     ssm = boto3.client('ssm')
     with Stubber(ssm) as stubber, \
       patch('set_tags.utils.get_marketplace_customer_id') as customer_id_mock, \
@@ -19,4 +19,41 @@ class TestGetMarketplaceTags(unittest.TestCase):
           {'Key': 'synapse:marketplaceProductCode', 'Value': 'mkt-prod-1234'},
           {'Key': 'synapse:marketplaceCustomerId', 'Value': 'mkt-cust-1234'}
         ]
+        self.assertListEqual(result, expected)
+
+  def test_no_product_code_tag(self):
+    ssm = boto3.client('ssm')
+    with Stubber(ssm) as stubber, \
+      patch('set_tags.utils.get_marketplace_customer_id') as customer_id_mock, \
+      patch('set_tags.utils.get_ssm_parameter') as ssm_param_mock:
+        customer_id_mock.return_value = ""
+        ssm_param_mock.return_value = "mkt-prod-1234"
+        result = utils.get_marketplace_tags(1234567)
+        expected = [
+          {'Key': 'synapse:marketplaceProductCode', 'Value': 'mkt-prod-1234'}
+        ]
+        self.assertListEqual(result, expected)
+
+  def test_no_customer_id_tag(self):
+    ssm = boto3.client('ssm')
+    with Stubber(ssm) as stubber, \
+      patch('set_tags.utils.get_marketplace_customer_id') as customer_id_mock, \
+      patch('set_tags.utils.get_ssm_parameter') as ssm_param_mock:
+        customer_id_mock.return_value = "mkt-cust-1234"
+        ssm_param_mock.return_value = ""
+        result = utils.get_marketplace_tags(1234567)
+        expected = [
+          {'Key': 'synapse:marketplaceCustomerId', 'Value': 'mkt-cust-1234'}
+        ]
+        self.assertListEqual(result, expected)
+
+  def test_no_tags(self):
+    ssm = boto3.client('ssm')
+    with Stubber(ssm) as stubber, \
+      patch('set_tags.utils.get_marketplace_customer_id') as customer_id_mock, \
+      patch('set_tags.utils.get_ssm_parameter') as ssm_param_mock:
+        customer_id_mock.return_value = ""
+        ssm_param_mock.return_value = ""
+        result = utils.get_marketplace_tags(1234567)
+        expected = []
         self.assertListEqual(result, expected)
