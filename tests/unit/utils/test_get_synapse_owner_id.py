0ea6b90d9e38a2db681d3f003329e395bdb288ae
Khai Do <3697686+zaro0508@users.noreply.github.com>
2025-09-24 17:13:10 -0700

    [IT-4483] Refactor get_synapse_owner_id (#63)
    Refactor get_synapse_owner_id method to get the owner ID from both synapse::ownerId and aws:servicecatalog:provisioningPrincipalArn tags with the former as the preferred option and the latter as the fallback option.
    
    This implements the following proposal in IT-4483:
    ```
    For a SC product Updates action the lambda currently also gets the synapse id
    from the aws:servicecatalog:provisioningPrincipalArn tag.  We would need to
    refactor the lambda to get the synapse id from thesynapse:owerId tag instead.
    Then the lambda will generate and apply all of the synapse prefixed tags with the
    new owner info. 
    ```

--- tests/unit/utils/test_get_synapse_owner_id.py
+++ tests/unit/utils/test_get_synapse_owner_id.py
@@ -5,36 +5,56 @@ from set_tags import utils
 
 class TestGetSynapseOwnerId(unittest.TestCase):
 
-  def test_list_tag_present(self):
-    tags = [
-      {'Key': 'heresatag', 'Value': 'heresatagvalue'},
-      {'Key': 'theresatag', 'Value': 'theresatagvalue'},
-      {'Key': 'aws:servicecatalog:provisioningPrincipalArn', 'Value': 'foo/bar'}
-    ]
-    result = utils.get_synapse_owner_id(tags)
-    self.assertEqual(result, 'bar')
-
-  def test_list_tag_missing(self):
-    with self.assertRaises(ValueError):
-      tags = [
-        {'Key': 'heresatag', 'Value': 'heresatagvalue'},
-        {'Key': 'theresatag', 'Value': 'theresatagvalue'}
-      ]
-      utils.get_synapse_owner_id(tags)
-
-  def test_dict_tag_present(self):
-    tags = {
-      'heresatag': 'heresatagvalue',
-      'theresatag':'theresatagvalue',
-      'aws:servicecatalog:provisioningPrincipalArn':'foo/bar'
-    }
-    result = utils.get_synapse_owner_id(tags)
-    self.assertEqual(result, 'bar')
-
-  def test_dict_tag_missing(self):
-    with self.assertRaises(ValueError):
-      tags = {
-        'heresatag': 'heresatagvalue',
-        'theresatag': 'theresatagvalue',
+  def test_dict_with_both_keys(self):
+      data = {
+          "synapse:ownerId": "1234567",
+          "aws:servicecatalog:provisioningPrincipalArn": "arn:aws:sts::111111111:assumed-role/ServiceCatalogEndusers/378505"
       }
-      utils.get_synapse_owner_id(tags)
+      self.assertEqual(utils.get_synapse_owner_id(data), "1234567")
+
+  def test_dict_with_only_owner(self):
+      data = {"synapse:ownerId": "1234567"}
+      self.assertEqual(utils.get_synapse_owner_id(data), "1234567")
+
+  def test_dict_with_only_principal(self):
+      data = {"aws:servicecatalog:provisioningPrincipalArn": "arn:aws:sts::111111111:assumed-role/ServiceCatalogEndusers/378505"}
+      self.assertEqual(
+          utils.get_synapse_owner_id(data),
+          "378505"
+      )
+
+  def test_dict_with_no_keys(self):
+      data = {"foo": "bar"}
+      self.assertIsNone(utils.get_synapse_owner_id(data))
+
+  def test_list_with_both_keys(self):
+      data = [
+          {"Key": "aws:servicecatalog:provisioningPrincipalArn", "Value": "arn:aws:sts::111111111:assumed-role/ServiceCatalogEndusers/378505"},
+          {"Key": "synapse:ownerId", "Value": "1234567"},
+      ]
+      self.assertEqual(utils.get_synapse_owner_id(data), "1234567")
+
+  def test_list_with_only_owner(self):
+      data = [{"Key": "synapse:ownerId", "Value": "1234567"}]
+      self.assertEqual(utils.get_synapse_owner_id(data), "1234567")
+
+  def test_list_with_only_principal(self):
+      data = [{"Key": "aws:servicecatalog:provisioningPrincipalArn", "Value": "arn:aws:sts::111111111:assumed-role/ServiceCatalogEndusers/378505"}]
+      self.assertEqual(
+          utils.get_synapse_owner_id(data),
+          "378505"
+      )
+
+  def test_list_with_no_keys(self):
+      data = [{"Key": "foo", "Value": "bar"}]
+      self.assertIsNone(utils.get_synapse_owner_id(data))
+
+  def test_empty_dict(self):
+      self.assertIsNone(utils.get_synapse_owner_id({}))
+
+  def test_empty_list(self):
+      self.assertIsNone(utils.get_synapse_owner_id([]))
+
+  def test_invalid_type(self):
+      with self.assertRaises(TypeError):
+          utils.get_synapse_owner_id("not a dict or list")
