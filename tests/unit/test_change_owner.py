718840cc61c2a3e4fc23e1cd8c89db4059badce0
xschildw <x.schildwachter@sagebase.org>
2026-04-16 21:33:04 -0700

    Added test

--- tests/unit/test_change_owner.py
+++ tests/unit/test_change_owner.py
@@ -8,7 +8,7 @@ class TestUpdateBucketPolicy(unittest.TestCase):
             "Statement": [
                 {
                     "Principal": {
-                        "AWS": [
+                        "AWS": [ # This case tests when the target user ID appears multiple times in the policy
                             "arn:aws:sts::111111111111:assumed-role/ServiceCatalogEndusers/1234567",
                             "arn:aws:sts::111111111111:assumed-role/ServiceCatalogEndusers/1234567",
                             "arn:aws:iam::111111111111:role/SomeOtherRole",
@@ -95,12 +95,12 @@ class TestUpdateBucketPolicy(unittest.TestCase):
         )
 
 
-    def test_principal_array(self):
+    def test_aws_principal_array(self):
         policy = {
             "Statement": [
                 {
                     "Principal": {
-                        "AWS": [
+                        "AWS": [ # This case tests when the AWS principal is an array
                             "arn:aws:sts::111111111111:assumed-role/ServiceCatalogEndusers/1234567",
                             "arn:aws:iam::111111111111:role/SomeOtherRole",
                         ]
@@ -128,11 +128,11 @@ class TestUpdateBucketPolicy(unittest.TestCase):
         )
         self.assertEqual(policy["Statement"][1]["Principal"], "*")
 
-    def test_principal_string(self):
+    def test_target_not_found_string(self):
         policy = {
             "Statement": [
                 {
-                    "Principal": {
+                    "Principal": { # This case tests when the AWS principal is a string instead of an array
                         "AWS": "arn:aws:sts::111111111111:assumed-role/ServiceCatalogEndusers/1234567"
                     }
                 },
@@ -156,3 +156,22 @@ class TestUpdateBucketPolicy(unittest.TestCase):
             ]
         )
         self.assertEqual(policy["Statement"][1]["Principal"], "*")
+
+
+    def test_principal_string(self):
+        policy = {
+            "Statement": [
+                {
+                    "Principal": "*"
+                }
+            ]
+        }
+
+        updated = update_bucket_policy(
+            bucket_policy=policy,
+            target_user_id="1234567",
+            new_assumed_role_arn="arn:aws:sts::111111111111:assumed-role/ServiceCatalogEndusers/7654321",
+        )
+
+        self.assertFalse(updated)
+        self.assertEqual(policy["Statement"][0]["Principal"], "*")
